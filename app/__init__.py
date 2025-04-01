0fa5de059a2a67762361af0b527eea399d3fba83
tahaali73 <diytech960@gmail.com>
2025-04-01 10:36:30 -0700

    added open ai model, created mongodb database for repeatable question. compeleted but still there is room for improvement i am leaving it here because of i have to learn other things more usefull

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from openai import OpenAI
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb+srv://diytech960:Qmobile888@cluster0.q0ffqok.mongodb.net/chatgpt"
+mongo = PyMongo(app)
+client = OpenAI(api_key="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",base_url="https://openrouter.ai/api/v1")
 
 from app import route
--- app/route.py
+++ app/route.py
@@ -1,16 +1,35 @@
-from app import app
+from app import app, mongo, client
 from flask import render_template, request, jsonify
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    chats = mongo.db.chat.find({})
+    mychats = [chat for chat in chats]
+    return render_template("index.html", mychats=mychats)
 
 @app.route('/api',methods=['GET', 'POST'])
 def qa():
     if request.method == "POST":  
-        question=request.get_json()
-        data = {"result":question['question']}
-        print(data)
-        return jsonify(data)
+        question=request.json.get("question")
+        chat = mongo.db.chat.find_one({"question": question})
+        if chat:
+            data = {"result":f"{chat['answer']}"}
+            return jsonify(data)
+        else:
+            
+            completion = client.chat.completions.create(
+            
+            model="openai/gpt-4o",
+            messages=[
+            {
+            "role": "user",
+            "content": "What is the meaning of life?"
+            }])
+            print(completion.choices[0].message.content)
+
+            
+            mongo.db.chat.insert_one({"question": question, "answer": "answer from open ai"})
+            return jsonify(data)
+    data = {"result":"this out of db question"}
     
     return jsonify(data)
