28d3cb3320db56ee148ba6054fb502e16623f7f5
tahaali73 <diytech960@gmail.com>
2025-04-01 02:11:55 -0700

    changing ui on click and fetching post request from routes.py to html using javascript

--- app/route.py
+++ app/route.py
@@ -1,6 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/api',methods=['GET', 'POST'])
+def qa():
+    if request.method == "POST":  
+        question=request.get_json()
+        data = {"result":question['question']}
+        print(data)
+        return jsonify(data)
+    
+    return jsonify(data)
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,22 @@
-console.log("Script is working!");
+
+async function postData(url="",data={}) {
+    const responce = await fetch(url, {
+        method: "POST", headers: {
+            "Content-Type": "application/json",
+        }, body: JSON.stringify(data),
+    });
+    return responce.json();
+    
+}
+
+sendButton.addEventListener("click", async()=>{
+    questionInput = document.getElementById("questionInput").value;
+    document.getElementById("questionInput").value="";
+    document.querySelector(".right2.flex-1.flex.flex-col.overflow-hidden").style.display = "block"
+    document.querySelector(".right1.flex-1.flex.flex-col.overflow-hidden").style.display = "none"
+
+    question2.innerHTML = questionInput;
+    
+    let result = await postData("/api",{"question":questionInput})
+    solution.innerHTML = result.result
+})
